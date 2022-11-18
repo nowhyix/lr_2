94b943e7199dfa9fa34f50460a03ea5de97d7f99
Anny <nowhyix@mail.ru>
2022-11-18 10:51:37 +1000

    лаба 2

---
+++ lr_2 KP.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char* c = new char[30];
+    int cou = 0;
+    int len = 0;
+    bool flag;
+    for (int i = 1; i <= 5; i++) {
+        cin >> c;
+        while (c[len]) {
+            len++;
+        }
+        flag = true;
+
+        int j = 0;
+        while ((j < len / 2) && flag) {
+            if (c[j] != c[len - 1 - j])
+                flag = false;
+            j++;
+        }
+        if (flag) {
+            cou++;
+        }
+    }
+    cout << cou;
+    delete c;
+    return 0;
+}
